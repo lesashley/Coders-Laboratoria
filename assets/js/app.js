1ea7b74839afbbef300441e6c9adf2ecbd538bce
Leslie A. Lopez <lesashley56@gmail.com>
2017-04-04 10:56:51 -0500

    Css terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,55 +2,52 @@ function Pictures() {
   this.Chile4 = [];
   this.addChile4 = function (archivo,nombre) {
     this.Chile4.push({
-      promocion : "4-chile",
+      promocion : "chile-4",
       file : archivo,
-      name : nombre
-    });
+      name : nombre });
   }
   this.Lima5 = [];
   this.addLima5 = function (archivo,nombre) {
     this.Lima5.push({
-      promocion : "5-lima",
+      promocion : "lima-5",
       name : nombre,
-      file : archivo
-    });
+      file : archivo });
   }
   this.Lima6 = [];
   this.addLima6 = function (archivo,nombre) {
     this.Lima6.push({
-      promocion : "6-lima",
+      promocion : "lima-6",
       name : nombre,
-      file : archivo
-    });
+      file : archivo });
   }
 }
 
 var picture = new Pictures();
 
-picture.addChile4("anais-araya .jpg","anais-araya");
-picture.addChile4("andrea-miranda.jpg","andrea-miranda");
-picture.addChile4("berenisse-ríos.jpg","berenisse-ríos");
-picture.addChile4("caterina-da-silva.jpg","caterina-da-silva");
-picture.addChile4("daniela-sanchez.jpg","daniela-sanchez");
-picture.addChile4("francisca-ojeda.jpg","francisca-ojeda");
-picture.addChile4("katerine-sandoval.jpg","katerine-sandoval");
+picture.addChile4("anais-araya .jpg","Anais Araya");
+picture.addChile4("andrea-miranda.jpg","Andrea Miranda");
+picture.addChile4("berenisse-ríos.jpg","Berenisse Ríos");
+picture.addChile4("caterina-da-silva.jpg","Caterina Da Silva");
+picture.addChile4("daniela-sanchez.jpg","Daniela Sanchez");
+picture.addChile4("francisca-ojeda.jpg","Francisca Ojeda");
+picture.addChile4("katerine-sandoval.jpg","Katerine Sandoval");
 
-picture.addLima5("andrea-cabrera.jpg","andrea-cabrera");
-picture.addLima5("ariadna-izaguirre.jpg","ariadna-izaguirre");
-picture.addLima5("carito-juarez.jpg","carito-juarez");
-picture.addLima5("cristy-castro.jpg","cristy-castro");
-picture.addLima5("karol-orrillo.jpg","karol-orrillo");
-picture.addLima5("paola-ortiz.jpg","paola-ortiz");
-picture.addLima5("teresa-lara.jpg","teresa-lara");
+picture.addLima5("andrea-cabrera.jpg","Andrea Cabrera");
+picture.addLima5("ariadna-izaguirre.jpg","Ariadna Izaguirre");
+picture.addLima5("carito-juarez.jpg","Carito Juarez");
+picture.addLima5("cristy-castro.jpg","Cristy Castro");
+picture.addLima5("karol-orrillo.jpg","Karol Orrillo");
+picture.addLima5("paola-ortiz.jpg","Paola Ortiz");
+picture.addLima5("teresa-lara.jpg","Teresa Lara");
 
-picture.addLima6("arantza-burga.jpg","arantza-burga");
-picture.addLima6("daguiana-revoredo.jpg","daguiana-revoredo");
-picture.addLima6("elizabeth-condori.jpg","elizabeth-condori");
-picture.addLima6("grecia-rayme.jpg","grecia-rayme");
-picture.addLima6("janine-vega.jpg","janine-vega");
-picture.addLima6("michelle-more.jpg","michelle-more");
-picture.addLima6("mishel-velasquez.jpg","mishel-velasquez");
-picture.addLima6("rosario-felix.jpg","rosario-felix");
+picture.addLima6("arantza-burga.jpg","Arantza Burga");
+picture.addLima6("daguiana-revoredo.jpg","Daguiana Revoredo");
+picture.addLima6("elizabeth-condori.jpg","Elizabeth Condori");
+picture.addLima6("grecia-rayme.jpg","Grecia Rayme");
+picture.addLima6("janine-vega.jpg","Janine Vega");
+picture.addLima6("michelle-more.jpg","Michelle More");
+picture.addLima6("mishel-velasquez.jpg","Mishel Velasquez");
+picture.addLima6("rosario-felix.jpg","Rosario Felix");
 
 
 var chile4 = document.getElementById("chile4");
@@ -59,10 +56,10 @@ var lima6 = document.getElementById("lima6");
 
 function crearImagen(prom,src,alt,parent) {
   var imagen = document.createElement("img");
-  imagen.classList.add(prom);
   imagen.classList.add("estilo");
   imagen.src="assets/images/"+prom+"/"+src;
   imagen.alt=alt;
+  imagen.title = alt;
   parent.appendChild(imagen);
 }
 

5d7630e7b4f49e0bbcc2f6a8aeca85173e0d5f5a
Leslie A. Lopez <lesashley56@gmail.com>
2017-04-04 12:23:19 -0500

    Css terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,76 +1,57 @@
-function Pictures() {
-  this.Chile4 = [];
-  this.addChile4 = function (archivo,nombre) {
-    this.Chile4.push({
-      promocion : "chile-4",
+function Pictures(){
+  this.coders = [];
+  this.addCoders = function (prom,archivo,nombre){
+    this.coders.push({
+      promocion : prom,
       file : archivo,
-      name : nombre });
-  }
-  this.Lima5 = [];
-  this.addLima5 = function (archivo,nombre) {
-    this.Lima5.push({
-      promocion : "lima-5",
-      name : nombre,
-      file : archivo });
-  }
-  this.Lima6 = [];
-  this.addLima6 = function (archivo,nombre) {
-    this.Lima6.push({
-      promocion : "lima-6",
-      name : nombre,
-      file : archivo });
+      name : nombre
+    });
   }
 }
 
 var picture = new Pictures();
 
-picture.addChile4("anais-araya .jpg","Anais Araya");
-picture.addChile4("andrea-miranda.jpg","Andrea Miranda");
-picture.addChile4("berenisse-ríos.jpg","Berenisse Ríos");
-picture.addChile4("caterina-da-silva.jpg","Caterina Da Silva");
-picture.addChile4("daniela-sanchez.jpg","Daniela Sanchez");
-picture.addChile4("francisca-ojeda.jpg","Francisca Ojeda");
-picture.addChile4("katerine-sandoval.jpg","Katerine Sandoval");
+picture.addCoders("chile-4","anais-araya .jpg","Anais Araya");
+picture.addCoders("chile-4","andrea-miranda.jpg","Andrea Miranda");
+picture.addCoders("chile-4","berenisse-ríos.jpg","Berenisse Ríos");
+picture.addCoders("chile-4","caterina-da-silva.jpg","Caterina Da Silva");
+picture.addCoders("chile-4","daniela-sanchez.jpg","Daniela Sanchez");
+picture.addCoders("chile-4","francisca-ojeda.jpg","Francisca Ojeda");
+picture.addCoders("chile-4","katerine-sandoval.jpg","Katerine Sandoval");
 
-picture.addLima5("andrea-cabrera.jpg","Andrea Cabrera");
-picture.addLima5("ariadna-izaguirre.jpg","Ariadna Izaguirre");
-picture.addLima5("carito-juarez.jpg","Carito Juarez");
-picture.addLima5("cristy-castro.jpg","Cristy Castro");
-picture.addLima5("karol-orrillo.jpg","Karol Orrillo");
-picture.addLima5("paola-ortiz.jpg","Paola Ortiz");
-picture.addLima5("teresa-lara.jpg","Teresa Lara");
+picture.addCoders("lima-5","andrea-cabrera.jpg","Andrea Cabrera");
+picture.addCoders("lima-5","ariadna-izaguirre.jpg","Ariadna Izaguirre");
+picture.addCoders("lima-5","carito-juarez.jpg","Carito Juarez");
+picture.addCoders("lima-5","cristy-castro.jpg","Cristy Castro");
+picture.addCoders("lima-5","karol-orrillo.jpg","Karol Orrillo");
+picture.addCoders("lima-5","paola-ortiz.jpg","Paola Ortiz");
+picture.addCoders("lima-5","teresa-lara.jpg","Teresa Lara");
 
-picture.addLima6("arantza-burga.jpg","Arantza Burga");
-picture.addLima6("daguiana-revoredo.jpg","Daguiana Revoredo");
-picture.addLima6("elizabeth-condori.jpg","Elizabeth Condori");
-picture.addLima6("grecia-rayme.jpg","Grecia Rayme");
-picture.addLima6("janine-vega.jpg","Janine Vega");
-picture.addLima6("michelle-more.jpg","Michelle More");
-picture.addLima6("mishel-velasquez.jpg","Mishel Velasquez");
-picture.addLima6("rosario-felix.jpg","Rosario Felix");
+picture.addCoders("lima-6","arantza-burga.jpg","Arantza Burga");
+picture.addCoders("lima-6","daguiana-revoredo.jpg","Daguiana Revoredo");
+picture.addCoders("lima-6","elizabeth-condori.jpg","Elizabeth Condori");
+picture.addCoders("lima-6","grecia-rayme.jpg","Grecia Rayme");
+picture.addCoders("lima-6","janine-vega.jpg","Janine Vega");
+picture.addCoders("lima-6","michelle-more.jpg","Michelle More");
+picture.addCoders("lima-6","mishel-velasquez.jpg","Mishel Velasquez");
+picture.addCoders("lima-6","rosario-felix.jpg","Rosario Felix");
 
 
-var chile4 = document.getElementById("chile4");
-var lima5 = document.getElementById("lima5");
-var lima6 = document.getElementById("lima6");
+var chile4 = document.getElementById("chile-4");
+var lima5 = document.getElementById("lima-5");
+var lima6 = document.getElementById("lima-6");
 
-function crearImagen(prom,src,alt,parent) {
+function crearImagen(prom,src,alt) {
   var imagen = document.createElement("img");
   imagen.classList.add("estilo");
   imagen.src="assets/images/"+prom+"/"+src;
   imagen.alt=alt;
   imagen.title = alt;
-  parent.appendChild(imagen);
+  document.getElementById(prom).appendChild(imagen);
 }
 
-picture.Chile4.forEach(function (e) {
-  crearImagen(e.promocion,e.file,e.name,chile4);
-});
-picture.Lima5.forEach(function (e) {
-  crearImagen(e.promocion,e.file,e.name,lima5);
-});
-picture.Lima6.forEach(function (e) {
-  crearImagen(e.promocion,e.file,e.name,lima6);
+picture.coders.forEach(function (e) {
+  crearImagen(e.promocion,e.file,e.name)
 });
 
 var select = document.getElementById("select");
